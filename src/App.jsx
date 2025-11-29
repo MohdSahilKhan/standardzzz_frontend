38283950220df8d61cbf449939efb14566652687
Sahil Khan <sahil.khan@deeporion.com>
2025-11-29 10:44:46 +0530

    router issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
 
-        {/* DEFAULT ROUTE → OPEN SIGNUP */}
-        <Route path="/" element={<Signup />} />
+      {/* DEFAULT ROUTE → OPEN SIGNUP */}
+      <Route path="/" element={<Signup />} />
 
-        {/* SIGNUP PAGE */}
-        <Route path="/signup" element={<Signup />} />
+      {/* SIGNUP PAGE */}
+      <Route path="/signup" element={<Signup />} />
 
-        {/* LOGIN PAGE */}
-        <Route path="/login" element={<Login />} />
+      {/* LOGIN PAGE */}
+      <Route path="/login" element={<Login />} />
 
-        {/* TEMP HOME PAGE */}
-        <Route
-          path="/home"
-          element={
-            <h1 className="text-white text-center mt-10 text-3xl">
-              Welcome Home!
-            </h1>
-          }
-        />
+      {/* TEMP HOME PAGE */}
+      <Route
+        path="/home"
+        element={
+          <h1 className="text-white text-center mt-10 text-3xl">
+            Welcome Home!
+          </h1>
+        }
+      />
 
-      </Routes>
-    </BrowserRouter>
+    </Routes>
   );
 }
